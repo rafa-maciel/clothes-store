aac5e6f5bd505b2d8d9f14812b5889da4905bd29
rafamaciel <rafael.macielf@gmail.com>
2019-07-24 09:29:24 -0300

    started project with spring boot (Web, JPA and DevTools)

---
+++ src/main/java/com/rmaciel/clothesstore/ClothesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.rmaciel.clothesstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesStoreApplication.class, args);
+	}
+
+}
